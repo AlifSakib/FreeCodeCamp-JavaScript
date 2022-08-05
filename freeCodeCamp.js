e6da34c497cbb857f08d89f11e972f1a52439a8e
AlifSakib <alifsakib@gmail.com>
2022-08-05 15:47:35 +0600

    Coding

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,3 +1,5 @@
+
+// !Data Types
 /* Data Types :
 undefined, null, boolean, string, symbol, number and object
 */
@@ -10,17 +12,89 @@ let ourName = "Oh-My-Posh"
 
 const p = 3.1416; 
 
-// Initializing Variables / Assignment Operator :
+// !Initializing Variables / Assignment Operator :
 
 var a = 9 ; //now a is 9 because be assigned 9 into a by using equal sign operator
 
-// Uninitialized Variables :
+// !Uninitialized Variables :
 
 var b;
 var c;
 var d;
 
-//Caswe Sensitivity in Variables :
+//!Caswe Sensitivity in Variables :
 
 // -> JavaScript is Case sessitive so take care of it .
+// -> Try to write variables using camelCase , Like : firstName .
 
+//!Adding Number
+
+var sum = 10 + 10;
+
+//!Substract Number
+
+var difference = 10 - 10;
+
+//!multiplicate Number
+
+var product = 8 * 0;
+
+//!Dividing Number
+
+var quotient = 66 / 33;
+
+// !Incrementing Number
+
+var myVar = 87;
+
+myVar = myVar + 1; myVar++ ; myVar--;
+
+//!Decimal Number
+
+var ourDecimal = 5.7;
+
+var myDecimal = 0.009;
+
+// !Multiply Decimals
+
+var product = 2.0 * 00;
+
+
+// !Dividing Decimals
+
+var product = 2.0 / 00;
+
+// !Reminders
+
+var reminder = 11 % 3;
+
+// !compound assignment with augmented addition
+
+var a = 3
+var b = 17
+var c = 12
+
+a += 12
+b += 9 
+c += 7
+
+// !compound assignment with augmented subtraction
+
+a -= 12
+b -= 9 
+c -= 7
+
+// !compound assignment with augmented multiplication 20:20
+// !compound assignment with augmented division 20:52
+// !declare string variables 21:21
+// !escaping literal quotes in strings 22:02
+// !quotinq strings with single quotes 22:43
+// !escape sequences in strings 25:19
+// !concatenating string with plus operator 26:48
+// !concatenating string with variables 29:01
+// !appending variables to strings 30:15
+// !find a length of string 31:12
+// !bracket notation to find first character in string 32:01
+// !bracket notation to find Nth character in string34:24
+// !bracket notation to find last character in string34:54
+// !word blanks 36:31
