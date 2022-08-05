86132c14bbce9413d99bfbf899818d81547c95d6
AlifSakib <alifsakib@gmail.com>
2022-08-05 15:05:37 +0600

    Coding

---
+++ freeCodeCamp.js
@@ -0,0 +1,26 @@
+/* Data Types :
+undefined, null, boolean, string, symbol, number and object
+*/
+
+var num1 = 4;
+
+var num1 = 3; 22 //Now num will be 3
+
+let ourName = "Oh-My-Posh"
+
+const p = 3.1416; 
+
+// Initializing Variables / Assignment Operator :
+
+var a = 9 ; //now a is 9 because be assigned 9 into a by using equal sign operator
+
+// Uninitialized Variables :
+
+var b;
+var c;
+var d;
+
+//Caswe Sensitivity in Variables :
+
+// -> JavaScript is Case sessitive so take care of it .
+
