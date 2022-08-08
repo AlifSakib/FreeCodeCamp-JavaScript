46d6fbb65fc400097c54b9725318383a494de141
AlifSakib <alifsakib@gmail.com>
2022-08-09 01:21:52 +0600

    Coding...

---
+++ JavaScript Basic Questions.js
@@ -0,0 +1,9 @@
+// *1. JavaScript is a high-level, interpreted programming language. It makes the web pages more Interactive.
+
+// *2. Variable is like a box that store thngs. there are five types of variables number => Premitive (String, Boolean, Null, undefined.) Non Premitive (Object)
+
+// *3. JavaScript keywords are reserved words and thry cannot be used as variable name and fucntion name.
+
+// *4.  Camel Case, Snake Case and Pascal Case are the three most common naming conventions.
+
+// *5 Variable names are Case Sensitive
